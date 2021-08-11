fdc9c2d8c207a9ae499df1f2cd63f8f18cc6ec10
Wiktor Wrona <wronawiktor@gmail.com>
2021-08-11 18:59:18 +0200

    Testowy program do włączenia diody

--- led/led_1.py
+++ led/led_1.py
@@ -1,3 +1,13 @@
 """
 Plik do włączania diody podłączonej do GPIO
 """
+print("hello!")
+#Pobranie biblioteki do sterowania GPIO
+import RPi.GPIO as GPIO
+#Używamy numeracji GPIO, a nie fizycznych pinów
+GPIO.setmode(GPIO.BCM)
+#Wyłączenie ostrzeżeń, które mogłby pojawić się przy ponownym otwieraniu pliku bez czyszczenia GPIO
+GPIO.setwarnings(False)
+#Definiujemy pod, który pin GPIO podłączona jest dioda
+pin = 5
+GPIO.output(pin, GPIO.HIGH)
