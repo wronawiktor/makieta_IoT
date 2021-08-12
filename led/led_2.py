8f6a14f26e3263ddf64764f458ca5bdc04cebc47
Wiktor Wrona <wronawiktor@gmail.com>
2021-08-12 12:14:38 +0200

    Zastąpienie odpowiedzi w if'ie listą zamiast orami. Dodanie nagłówka z licencją Dodanie zdefiniowania GPIO jako output

--- led/led_2.py
+++ led/led_2.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2021 Wiktor Wrona Company
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Plik do sterowania diodą podłączoną do wybranego GPIO
 """
@@ -10,11 +24,11 @@ import sys
 GPIO.setmode(GPIO.BCM)
 # Wyłączenie ostrzeżeń, które mogłby pojawić się przy ponownym otwieraniu pliku bez czyszczenia GPIO
 GPIO.setwarnings(False)
-
-# Definiujemy pod, który pin GPIO podłączona jest dioda
-pin = int(input("Podaj port GPIO pod który podłączona jest dioda (domyślnie: 5): "))
-
-# Podajemy zgodnie z preferencją użytkownika na wcześniej zdefiniowany pin
+# Definiujemy pod, który GPIO podłączona jest dioda
+PIN = int(input("Podaj port GPIO pod który podłączona jest dioda (domyślnie: 5): "))
+# Użycie u jako wyjściowy
+GPIO.setup(PIN, GPIO.OUT)
+# Podajemy zgodnie z preferencją użytkownika na wcześniej zdefiniowany PIN
 print("""
 Wybierz czynność:
 - Y - Włącz diodę
@@ -24,18 +38,17 @@ Wybierz czynność:
 while True:
     print()
     operacja = input("Czynność: ")
-    if operacja == ("Y" or "y"):
-        GPIO.output(pin, GPIO.HIGH)
+    if operacja in ["Y", "y"]:
+        GPIO.output(PIN, GPIO.HIGH)
         continue
-    elif operacja == ("N" or "n"):
-        GPIO.output(pin, GPIO.LOW)
+    elif operacja in ["N", "n"]:
+        GPIO.output(PIN, GPIO.LOW)
         continue
-    elif operacja == ("Q" or "q"):
+    elif operacja in ["Q", "q"]:
         GPIO.cleanup()
         break
     else:
         input("Błędna komenda! Wybierz jeszcze raz! (Naciśnij, aby kontynuować): ")
         # Czekaj na reakcję użytkownika
         continue
-
 sys.exit(0)
