933bad8deee207df5893e3a1d68f52c689af4cde
Wiktor Wrona <wronawiktor@gmail.com>
2021-08-12 17:33:47 +0200

    Obsługa przyjęcia wartości domyślnej w przypadku braku podania nr PINu Nowy prgram do migania diodą, posiada podstawowe zabezpieczenia np. przed podaniem litery jako liczby

--- led/led_3.py
+++ led/led_3.py
@@ -14,45 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Plik do sterowania diodą podłączoną do wybranego GPIO
+Plik do migania diodą podłączoną do wybranego GPIO
 """
 # Pobranie biblioteki do sterowania GPIO oraz sys
 import RPi.GPIO as GPIO
 import sys
+import time
 
 # Używamy numeracji GPIO, a nie fizycznych pinów
 GPIO.setmode(GPIO.BCM)
 # Wyłączenie ostrzeżeń, które mogłby pojawić się przy ponownym otwieraniu pliku bez czyszczenia GPIO
 GPIO.setwarnings(False)
 # Definiujemy pod, który GPIO podłączona jest dioda
-PIN = input("Podaj port GPIO pod który podłączona jest dioda (domyślnie: 6): ")
-if len(PIN) == 0:
-    PIN = 6
-else:
-    PIN = int(PIN)
+
+while True:
+    try:
+        PIN = input("Podaj port GPIO pod który podłączona jest dioda (domyślnie: 6): ")
+        if len(PIN) == 0:
+            PIN = 6
+            break
+        else:
+            PIN = int(PIN)
+            break
+    except ValueError:
+        print("Podaj poprawną liczbę!")
+    else:
+        break
+
+
+
+
 # Użycie u jako wyjściowy
 GPIO.setup(PIN, GPIO.OUT)
-# Podajemy zgodnie z preferencją użytkownika na wcześniej zdefiniowany PIN
-print("""
-Wybierz czynność:
-- Y - Włącz diodę
-- N - Wyłącz diodę
-- Q - Wyjdź z programu
-""")
+# Podajemy okres jednego mignięcia zgodnie z preferencją użytkownika
+
 while True:
-    print()
-    operacja = input("Czynność: ")
-    if operacja in ["Y", "y"]:
-        GPIO.output(PIN, GPIO.HIGH)
+    try:
+        okres = float(input("Podaj okres jednego mignięcia (w sekundach): "))
+    except ValueError:
+        print("Podaj poprawną liczbę!")
         continue
-    elif operacja in ["N", "n"]:
-        GPIO.output(PIN, GPIO.LOW)
-        continue
-    elif operacja in ["Q", "q"]:
-        GPIO.cleanup()
-        break
     else:
-        input("Błędna komenda! Wybierz jeszcze raz! (Naciśnij, aby kontynuować): ")
-        # Czekaj na reakcję użytkownika
-        continue
+        break
+
+print("Aby zakończyć działanie programu, naciśnij Ctrl + C")
+
+while True:
+    GPIO.output(PIN, GPIO.HIGH)
+    time.sleep(okres / 2)
+    GPIO.output(PIN, GPIO.LOW)
+    time.sleep(okres / 2)
+
+GPIO.cleanup()
 sys.exit(0)
