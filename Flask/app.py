f4521dfd1634d4ecc63d1d1859f4ccee9993027d
Wiktor Wrona <wronawiktor@gmail.com>
2021-09-06 12:57:41 +0200

    Relacy control from web server - working

--- Flask/app.py
+++ Flask/app.py
@@ -1,62 +1,58 @@
-from flask import Flask, request, url_for, redirect
-from datetime import datetime
 
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
+import sys
+sys.path.insert(1, '/home/pi/makieta_IoT/relays')
+import relay_control
+
+relays = {
+   26: {"name": "Relay 1", "state": "off"},
+   19: {"name": "Relay 2", "state": "off"}
+   }
+
+for rel in relays:
+   relay_control.off(rel)
 
 
-@app.route('/')
-def index():
-    time_now = datetime.now().strftime("%H:%M:%S")
-    menu = f'''
-    Go <a href="{ url_for('dodawanie', a = 5, b = 3)}">here</a> to substract 5 and 3<br>
-    Go <a href="{ url_for('another')}">here</a> to another tab<br>
-    Go <a href="{ url_for('addition')}">here</a> to add custom numbers tab<br>
-    '''
+@app.route("/")
+def main():
+   # For each pin, read the pin state and store it in the pins dictionary:
+   for relay in relays:
+      if relays[relay]['state'] == "on":
+         relay_control.on(relay)
+      else:
+         relay_control.off(relay)
 
-    return f'<h1>Hello World!: {time_now}</h1><br>{menu}'
+   # Put the pin dictionary into the template data dictionary:
+   templateData = {
+      'relays' : relays
+      }
+   # Pass the template data into the template main.html and return it to the user
+   return render_template('main.html', **templateData)
 
+# The function below is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+   global relays
+   # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # If the action part of the URL is "on," execute the code indented below:
+   if action == "on":
+      # Set the pin high:
+      relay_control.on(changePin)
+      relays[changePin]['state'] = "on"
 
-@app.route('/dodawanie/<a>/<b>', methods =['POST'])
-def dodawanie(a, b):
-    a = int(a)
-    b = int(b)
-    wynik = str(a + b)
-    return wynik
+   if action == "off":
+      relay_control.off(changePin)
+      relays[changePin]['state'] = "off"
 
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'relays' : relays
+   }
 
-@app.route('/addition', methods =['GET', 'POST'])
-def addition():
-
-    if request.method == 'GET':
-        body = f'''
-            <form id="addition_form" action="{url_for("addition")}" method="POST">
-                <label for="a">Liczba A</label>
-                <input type="text" id="a" name='a' value="5"<br>
-                <label for="b">Liczba B</label>
-                <input type="text" id="a" name='b' value="4"<br>
-                <input type="submit" value="Send">
-            </form>
-        '''
-        return body
-    else:
-        a = "5"
-        if "a" in request.form:
-            a = request.form['a']
-
-        b = "7"
-        if "b" in request.form:
-            b = request.form['b']
-    body = f'{a} and {b} addition'
-    return body
-
-
-@app.route('/another')
-def another():
-    body = """
-    <h1>Just another tab</h1>
-    """
-    return body
-
+   return render_template('main.html', **templateData)
+   #return redirect(url_for('main', **templateData))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+   app.run(host='0.0.0.0', port=5000, debug=True)
