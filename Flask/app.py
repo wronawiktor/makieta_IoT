903040771a1f020f8d197141a479c2aca7fe2c45
Wiktor Wrona <wronawiktor@gmail.com>
2021-09-09 23:37:56 +0200

    changed method used to control relays and diodes

--- Flask/app.py
+++ Flask/app.py
@@ -1,74 +1,64 @@
 
 from flask import Flask, render_template, request, url_for, redirect
+import RPi.GPIO as GPIO
 app = Flask(__name__)
 import sys
 sys.path.insert(1, '/home/pi/makieta_IoT')
 from relays import relay_control
+from led import led_control
 from lcd import drivers
 
-
-
-relays = {
-   26: {"name": "Relay_1", "state": 0},
-   19: {"name": "Relay_2", "state": 0}
-   }
-
-for rel in relays:
-   relay_control.off(rel)
-
+R1 = 26
+R2 = 19
+Red = 5
+Grn = 6
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   """
-   for relay in relays:
-      if relays[relay]['state'] == True:
-         relay_control.on(relay)
-      else:
-         relay_control.off(relay)
-   """
-   if request.method == 'POST':
-      test = request.form['Relay_1']
-      if test == True:
-         relay_control.on(26)
-      else:
-         relay_control.off(26)
+   # Read GPIO Status
+   R1_Sts = GPIO.input(R1)
+   R2_Sts = GPIO.input(R2)
+   Red_Sts = GPIO.input(Red)
+   Grn_Sts = GPIO.input(Grn)
 
-   # Put the pin dictionary into the template data dictionary:
+# First html, second python
    templateData = {
-      'relays' : relays
-
-      }
-   # Pass the template data into the template main.html and return it to the user
-   return render_template('main.html', Relay_1=Relay_1)
-
-
-
-"""
-# The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/<changePin>/<action>")
-def action(changePin, action):
-   global relays
-   # Convert the pin from the URL into an integer:
-   changePin = int(changePin)
-   # If the action part of the URL is "on," execute the code indented below:
-   if action == "on":
-      # Set the pin high:
-      relay_control.on(changePin)
-      relays[changePin]['state'] = "on"
-
-   if action == "off":
-      relay_control.off(changePin)
-      relays[changePin]['state'] = "off"
-
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'relays' : relays
+      'Relay_1': R1_Sts,
+      'Relay_2': R2_Sts,
+      'ledRed': Red_Sts,
+      'ledGrn': Grn_Sts,
    }
+   return render_template('index.html', **templateData)
 
-   return render_template('main.html', **templateData)
-   #return redirect(url_for('main', **templateData))
-"""
+
+@app.route("/<deviceName>/<action>")
+def action(deviceName, action):
+   if deviceName == 'Relay_1':
+      relay = R1
+      led = Red
+   if deviceName == 'Relay_2':
+      relay = R2
+      led = Grn
+
+   if action == "on":
+      relay_control.on(relay)
+      led_control.on(led)
+   if action == "off":
+      relay_control.off(relay)
+      led_control.off(led)
+
+   R1_Sts = GPIO.input(R1)
+   R2_Sts = GPIO.input(R2)
+   Red_Sts = GPIO.input(Red)
+   Grn_Sts = GPIO.input(Grn)
+
+   templateData = {
+      'Relay_1': R1_Sts,
+      'Relay_2': R2_Sts,
+      'ledRed': Red_Sts,
+      'ledGrn': Grn_Sts,
+   }
+   return render_template('index.html', **templateData)
 
 @app.route("/display/<Line1>/<Line2>")
 def display(Line1, Line2):
